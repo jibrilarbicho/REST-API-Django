1f8887cf337fe2c8ec989022450195e27fddf70b
Jibril Arbicho <jibrilarbicho2@gmail.com>
2024-01-31 23:34:07 +0300

    Creating JSON Response for Individual Elements

--- watchmate/watchlist_app/urls.py
+++ watchmate/watchlist_app/urls.py
@@ -1,4 +1,7 @@
-from watchlist_app.views import movie_list
+from watchlist_app.views import movie_list, movie_detail
 from django.urls import path
 
-urlpatterns = [path("list/", movie_list, name="movie_list")]
+urlpatterns = [
+    path("list/", movie_list, name="movie_list"),
+    path("<int:pk>", movie_detail, name="movie_detail"),
+]
--- watchmate/watchlist_app/views.py
+++ watchmate/watchlist_app/views.py
@@ -8,3 +8,13 @@ def movie_list(requset):
     movies = Movie.objects.all()
     data = {"movies": list(movies.values())}
     return JsonResponse(data)
+
+
+def movie_detail(request, pk):
+    movie = Movie.objects.get(pk=pk)
+    data = {
+        "name": movie.name,
+        "description": movie.description,
+        "active": movie.active,
+    }
+    return JsonResponse(data)
