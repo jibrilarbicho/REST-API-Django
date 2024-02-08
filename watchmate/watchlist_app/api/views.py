96b6b6e782a96820bc4558e8bee438ecc4e7cd45
Jibril Arbicho <jibrilarbicho2@gmail.com>
2024-02-08 10:08:00 +0300

    Serializers GET,PUT,DELETE Request

---
+++ watchmate/watchlist_app/api/views.py
@@ -0,0 +1,37 @@
+from watchlist_app.models import Movie
+from watchlist_app.api.serializers import Movieserilizer
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+
+@api_view(["GET", "POST"])
+def movie_list(request):
+    if request.method == "GET":
+        movies = Movie.objects.all()
+        serializer = Movieserilizer(movies, many=True)
+        return Response(serializer.data)
+    if request.method == "POST":
+        serializer = Movieserilizer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
+
+
+@api_view(["GET", "PUT", "DELETE"])
+def movie_detail(request, pk):
+    if request.method == "GET":
+        movie = Movie.objects.get(pk=pk)
+        serializer = Movieserilizer(movie)
+        return Response(serializer.data)
+    if request.method == "PUT":
+        movie = Movie.objects.get(pk=pk)
+        serializer = Movieserilizer(movie, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
+    if request.method == "DELETE":
+        movie = Movie.objects.get(pk=pk)
+        movie.delete()
+        return Response(status=204)
