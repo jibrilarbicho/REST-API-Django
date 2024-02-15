5fcc94f8bc4d144402727acb31e40cc36c1cba7a
Jibril Arbicho <jibrilarbicho2@gmail.com>
2024-02-15 10:17:27 +0300

    Temporary Login and Logout

--- watchmate/watchmate/urls.py
+++ watchmate/watchmate/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
 
@@ -21,4 +22,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("movie/", include("watchlist_app.api.urls")),
+    path("api-auth/", include("rest_framework.urls")),
 ]
